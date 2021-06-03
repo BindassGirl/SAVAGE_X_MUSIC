618f04115dfc01b988a707cf7f75d06dca504d68
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-06-03 15:58:37 +0530

    Create start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,25 +9,25 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups. Special thanks to this person [SHRVAN](https://t.me/shrvan42) For Support Join our group @SAVAGE_USERBOT.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hey, I'm  {bn}, 𝙸 𝙰𝙼 𝙰𝙳𝚅𝙰𝙽𝙲𝙴𝙳 𝙱𝙾𝚃 🤖 𝙲𝚁𝙴𝙰𝚃𝙴𝙳 𝙱𝚈 [𝚂𝙰𝙼𝙴𝙴𝚁](t.me/sameer_795) & [𝚂𝙷𝚁𝚅𝙰𝙽](t.me/shrvan42) ❤️  𝙵𝙾𝚁 𝙿𝙻𝙰𝚈𝙸𝙽𝙶 𝙼𝚄𝚂𝙸𝙲 🎶 𝙸𝙽 𝚈𝙾𝚄𝚁 𝚅𝙾𝙸𝙲𝙴 𝙲𝙷𝙰𝚃𝚂 𝙾𝙵 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙶𝚁𝙾𝚄𝙿𝚂 𝙰𝙽𝙳 𝙲𝙷𝙰𝙽𝙽𝙴𝙻𝚂.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/SAVAGE_USERBOT"
+                        "💖𝙲𝚁𝙴𝙰𝚃𝙾𝚁💖", url="https://t.me/SAMEER_795"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Creater⚡️", url="https://t.me/SAMEER_795"
+                        "💬𝚂𝚄𝙿𝙿𝙾𝚁𝚃💬", url="https://t.me/SAVAGE_USERBOT"
                     ),
                     InlineKeyboardButton(
-                        "Assistant⚡️", url="https://t.me/SAVAGE_MUSIC_BOT_ASSISTANT"
+                        "⚔️𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃⚔️", url="https://t.me/SAVAGE_MUSIC_BOT_ASSISTANT"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Add To Your Group⚡️", url="https://t.me/SAVAGE_MUSIC_BOT?startgroup=true"
+                        "➕ 𝙰𝙳𝙳 𝙼𝙴 𝙸𝙽 𝚄𝚁 𝙶𝚁𝙿", url="https://t.me/SAVAGE_MUSIC_BOT?startgroup=true"
                     )
                 ]
             ]
