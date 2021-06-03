6bb0f1ba40d8e2bd36238ba8ea92fc1b17ed8d48
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-03 15:01:45 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,20 +9,20 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @W2HSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups. Special thanks to this person [SHRVAN]{https://t.me/shrvan42}For Support Join our group @SAVAGE_USERBOT.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/W2HSupport"
+                        "Support⚡️", url="https://t.me/SAVAGE_USERBOT"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Creater⚡️", url="https://t.me/David99q"
+                        "Creater⚡️", url="https://t.me/SAMEER_795"
                     ),
                     InlineKeyboardButton(
-                        "Ravan102030⚡️", url="https://t.me/Ravan102030"
+                        "Assistant⚡️", url="https://t.me/SAVAGE_MUSIC_BOT_ASSISTANT"
                     )
                 ],
                 [
