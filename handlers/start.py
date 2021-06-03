c64233de64f7f1310b8642fdb4676ad3c05d8032
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-03 15:04:03 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,7 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups. Special thanks to this person [SHRVAN]{https://t.me/shrvan42}For Support Join our group @SAVAGE_USERBOT.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups. Special thanks to this person [SHRVAN](https://t.me/shrvan42) For Support Join our group @SAVAGE_USERBOT.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
